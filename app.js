f83f0fe3e272349e0827f89f499a5cb2226e87e3
ProfKSawka <ks2013@georgetown.edu>
2025-08-19 17:17:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// Copy the entire React component code from the artifact here
+// (The full PortersFiveForcesAnalyzer component)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PortersFiveForcesAnalyzer />);
